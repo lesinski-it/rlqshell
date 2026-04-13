ab9c3e2bb751e40ec13fdd4029979b82dc0f564c
Rafał Lesiński <ravczy1982@gmail.com>
2026-04-13 22:49:15 +0200

    chore: bump version to 2.8.0

--- rlqshell/app/constants.py
+++ rlqshell/app/constants.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # App metadata
 APP_NAME = "RLQShell"
-APP_VERSION = "2.7.9"
+APP_VERSION = "2.8.0"
 ORGANIZATION_NAME = "RLQShell"
 APP_AUTHOR = "Ravczy"
 APP_AUTHOR_WEBSITE = "https://www.lesinski.it"
